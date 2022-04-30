02f9f87bcf34249142d054992a2b3c26e62d3d49
emirgundem <emirgndem@gmail.com>
2022-04-30 23:30:23 +0300

    added deleteCardforUser

--- src/services/iyzico/index.js
+++ src/services/iyzico/index.js
@@ -27,4 +27,69 @@ const createUserAndCards = () => {
     })
 }
 
-createUserAndCards();
+
+const createCardForUser = () => {
+    Cards.createUserCard({
+        locale: Iyzipay.LOCALE.TR,
+        conversationId: nanoid(),
+        email: "email@email.com",
+        externalId: nanoid(),
+        cardUserKey: "IsVS4wxwo3OhVWFYdIouWopNMtk=",
+        card: {
+            cardAlias: "Kredi Kartım",
+            cardHolderName: "John Doe",
+            cardNumber: "5528790000000008",
+            expireMonth: "12",
+            expireYear: "2030"
+        }
+    }).then((result) => {
+        console.log(result);
+        Logs.logFile("2-cards- bir-kullanıcıya-kart-ekle", result)
+    }).catch((err) => {
+        console.log(err)
+        Logs.logFile("2-cards- bir-kullanıcıya-kart-ekle-hata", err)
+    })
+}
+
+
+
+const readCardOfUser = () => {
+    Cards.getUserCards({
+        locale: Iyzipay.LOCALE.TR,
+        conversationId: nanoid(),
+        cardUserKey: "IsVS4wxwo3OhVWFYdIouWopNMtk=",
+
+    }).then((result) => {
+        console.log(result);
+        Logs.logFile("3-cards- bir-kullanıcının-kartını-oku", result)
+    }).catch((err) => {
+        console.log(err)
+        Logs.logFile("3-cards- bir-kullanıcının-kartını-oku-hata", err)
+    })
+}
+
+
+
+const deleteCardOfAUser = () => {
+    Cards.deleteUserCard({
+        locale: Iyzipay.LOCALE.TR,
+        conversationId: nanoid(),
+        cardUserKey: "IsVS4wxwo3OhVWFYdIouWopNMtk=",
+        cardToken:"J6E1VpdklhARn1hjBZQeAQBnO9w="
+
+    }).then((result) => {
+        console.log(result);
+        Logs.logFile("4-cards- bir-kullanıcının-kartını-sil", result)
+    }).catch((err) => {
+        console.log(err)
+        Logs.logFile("4-cards- bir-kullanıcının-kartını-sil-hata", err)
+    })
+}
+
+//deleteCardOfAUser();
+readCardOfUser();
+
+
+//createUserAndCards();
+//createCardForUser();
+
